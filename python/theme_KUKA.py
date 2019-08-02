eb8f216c9cd92313bdc82c48c13870aa7a53768c
Phillip <phillipmay@videotron.ca>
2019-08-02 12:06:14 -0400

    Fixed $ and # keywords for KUKA

--- python/theme_KUKA.py
+++ python/theme_KUKA.py
@@ -42,22 +42,47 @@ repo_match(repository, match, name_movements, "movement")
 
 match = "BAS SET_KRLMSG EXISTS_KRLMSG CLEAR_KRLMSG TRIGGER DISTANCE SET_KRLDLG EXISTS_KRLDLG StrClear LOOP_MSG LOOP_CONT"
 repo_match(repository, match, name_builtInFcn, "built-in-fcn")
+#LOOP_MSG and LOOP_CONT need special cases because they are used like variables sometimes.
+match = r"\$LOOP_MSG"
+repo_match(repository, match, name_builtInFcn, "built-in-fcn")
+match = r"\$LOOP_CONT"
+repo_match(repository, match, name_builtInFcn, "built-in-fcn")
+
 
 match_type = "built-in-types"
 match = "DECL GLOBAL CONST INT REAL BOOL CHAR PRIO FRAME POS E6POS AXIS E6AXIS"
 repo_match(repository, match, name_builtInTypes, "built-in-types")
 
-match = "BASE_DATA TOOL_DATA $ADVANCE $TOOL $BASE $AXIS_ACT $ACT_EX_AX $EX_AX_DATA"
-match += " #INITMOV BWDSTART INITMOV VEL_PTP ACC_PTP"
+match = "BASE_DATA TOOL_DATA ADVANCE TOOL BASE AXIS_ACT ACT_EX_AX EX_AX_DATA"
+match += " INITMOV BWDSTART INITMOV VEL_PTP ACC_PTP"
 match += " ANOUT OUT IN ANIN ON OFF TRUE FALSE"
-match += " $ACC $VEL $APO C_DIS C_PTP"
-match += " $TIMER_STOP $TIMER_FLAG $TIMER"
-match += " $VEL.CP $ACT_BASE $APO.CPTP $APO.CDIS"
-match += " $VEL.ORI1 $VEL.ORI2 $ACC.ORI1 $ACC.ORI2"
+match += " ACC VEL APO C_DIS C_PTP"
+match += " TIMER_STOP TIMER_FLAG TIMER"
+match += " VEL.CP ACT_BASE APO.CPTP APO.CDIS"
+match += " VEL.ORI1 VEL.ORI2 ACC.ORI1 ACC.ORI2"
 
 # KUKA dollar sign does not work
 #match = match.replace('$','$$')
 repo_match(repository, match, name_builtInVar, "built-in-var")
+
+#Kuka variables can also be accessed with a # or a $
+for word in match.split():
+    matchKeyWord1 = '\\#'
+    matchKeyWord1 += word
+    matchKeyWord2 = '\\$'
+    matchKeyWord2 += word
+    repo_match(repository, matchKeyWord1, name_builtInVar, "built-in-var")
+    repo_match(repository, matchKeyWord2, name_builtInVar, "built-in-var")
+
+#matchlist = r"$ACC $ADVANCE $TOOL $BASE $AXIS_ACT $ACT_EX_AX $EX_AX_DATA #INITMOV $ACC $VEL $APO"
+#matchlist += r" $TIMER_STOP $TIMER_FLAG $TIMER $VEL.CP $ACT_BASE $APO.CPTP $APO.CDIS"
+#matchlist += r" $VEL.ORI1 $VEL.ORI2 $ACC.ORI1 $ACC.ORI2"
+
+#for word in matchlist.split():
+#    match = '\\'
+#    match += word
+#    #repo_match(repository, match, name_builtInVar, "built-in-var")
+
 
 match_type = "operator"
 match = r'\\b= < > + - , ( ) { } [ ]\\b'
